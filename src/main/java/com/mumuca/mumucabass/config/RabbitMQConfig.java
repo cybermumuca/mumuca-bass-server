e9caa1c275c2745dd32086fc7537990629a06c84
sx1n <samuellaurindodelima2004@gmail.com>
2024-11-20 11:37:29 -0300

    add track download status queue

--- src/main/java/com/mumuca/mumucabass/config/RabbitMQConfig.java
+++ src/main/java/com/mumuca/mumucabass/config/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.mumuca.mumucabass.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -20,6 +21,12 @@ public class RabbitMQConfig {
     @Value("${mq.queues.track-download-dlq}")
     private String trackDownloadDeadLetterQueue;
 
+    @Value("${mq.queues.track-download-status}")
+    private String trackDownloadStatusQueue;
+
+    @Value("${mq.queues.track-download-status-dlq}")
+    private String trackDownloadStatusDeadLetterQueue;
+
     @Value("${mq.exchanges.track}")
     private String trackExchange;
 
@@ -31,6 +38,12 @@ public class RabbitMQConfig {
 
     @Value("${mq.routing-keys.track-download-dlq}")
     private String trackDownloadDeadLetterRoutingKey;
+
+    @Value("${mq.routing-keys.track-download-status}")
+    private String trackDownloadStatusRoutingKey;
+
+    @Value("${mq.routing-keys.track-download-status-dlq}")
+    private String trackDownloadStatusDeadLetterRoutingKey;
 
     @Bean
     public Queue trackDownloadQueue() {
@@ -44,6 +57,20 @@ public class RabbitMQConfig {
     @Bean
     public Queue trackDownloadDeadLetterQueue() {
         return new Queue(trackDownloadDeadLetterQueue, true);
+    }
+
+    @Bean
+    public Queue trackDownloadStatusQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", trackDeadLetterExchange);
+        args.put("x-dead-letter-routing-key", trackDownloadStatusDeadLetterRoutingKey);
+        args.put("x-message-ttl", 10000);// 10 seconds
+        return new Queue(trackDownloadStatusQueue, true, false, false, args);
+    }
+
+    @Bean
+    public Queue trackDownloadStatusDeadLetterQueue() {
+        return new Queue(trackDownloadStatusDeadLetterQueue, true);
     }
 
     @Bean
@@ -73,14 +100,44 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public Binding trackDownloadStatusBinding(Queue trackDownloadStatusQueue, DirectExchange trackExchange) {
+        return BindingBuilder
+                .bind(trackDownloadStatusQueue)
+                .to(trackExchange)
+                .with(trackDownloadStatusRoutingKey);
+    }
+
+    @Bean
+    public Binding trackDownloadStatusDeadLetterBinding(Queue trackDownloadStatusDeadLetterQueue, DirectExchange trackDeadLetterExchange) {
+        return BindingBuilder
+                .bind(trackDownloadStatusDeadLetterQueue)
+                .to(trackDeadLetterExchange)
+                .with(trackDownloadStatusDeadLetterRoutingKey);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        template.setMessageConverter(jackson2JsonMessageConverter());
         template.setBeforePublishPostProcessors(message -> {
             message.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT);
             return message;
         });
 
         return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
+        return factory;
     }
 }
