459aadba26446226de4b526b90e17441f430a8fd
sx1n <samuellaurindodelima2004@gmail.com>
2024-11-22 20:21:16 -0300

    add mumuca bass storage ms

---
+++ mumuca-bass-storage/src/test/java/com/mumuca/mumucabassstorage/MumucaBassStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumuca.mumucabassstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumucaBassStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
