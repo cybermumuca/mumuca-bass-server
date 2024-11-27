334bbf9e6eb2bf20bf8ff2347a3496b10393f109
sx1n <samuellaurindodelima2004@gmail.com>
2024-11-27 00:03:26 -0300

    remove scheduler config

--- src/main/java/com/mumuca/mumucabass/MumucaBassApplication.java
+++ src/main/java/com/mumuca/mumucabass/MumucaBassApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
-@EnableScheduling
 @EnableRabbit
 public class MumucaBassApplication {
     public static void main(String[] args) {
--- src/main/java/com/mumuca/mumucabass/config/SchedulerConfig.java
+++
@@ -1,6 +0,0 @@
-package com.mumuca.mumucabass.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SchedulerConfig {}
